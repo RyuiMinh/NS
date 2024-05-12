f421c635533cf981fa8e1eabf01c8b5d90f5c5b7
Begauphieuluu <166026868+Begauphieuluu@users.noreply.github.com>
2024-05-12 15:15:44 +0700

    Update win10.sh

--- win10.sh
+++ win10.sh
@@ -5,15 +5,13 @@ nohup ./ngrok tcp 5900 &>/dev/null &
 echo Ngrok Connect!
 echo -------------------------------
 sudo apt update -y > /dev/null 2>&1
-echo "Installing QEMU (2-3m)..."
+echo "Installing QEMU..."
 sudo apt install qemu-system-x86 curl -y > /dev/null 2>&1
 echo Downloading Windows Disk...
 curl -L -o w10x64.img https://bit.ly/akuhnetW10x64
-echo "Windows 10 x64 Lite On Google Colab"
+echo "Windows 10 x64 Lite"
 echo Your VNC IP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
-echo "Note: Use Right-Click Or Ctrl+C To Copy"
-echo "Please Keep Colab Tab Open, Maximum Time 12h"
-echo Script by fb.com/thuong.hai.581
-echo Starting Windows xxxz...
+echo Script by fb.me/LeVuHg
+echo Starting Windows...
 sudo qemu-system-x86_64 -vnc :0 -hda w10x64.img -smp cores=2 -m 2012M -machine usb=on -device usb-tablet
