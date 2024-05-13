7236a7eb6da5a024343f661752f5981f3bcd9527
Begauphieuluu <166026868+Begauphieuluu@users.noreply.github.com>
2024-05-13 20:14:09 +0700

    Update jetbrains7.sh

--- jetbrains7.sh
+++ jetbrains7.sh
@@ -1,7 +1,7 @@
 wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz
 tar -xvzf ngrok-v3-stable-linux-amd64.tgz
 ./ngrok authtoken 2caMIyH98jojFijfUDA9HHKZDZ0_2naziucGo1SjGJ4xWL8QM
-nohup ./ngrok tcp 5900 >/dev/null 2>&1
+nohup ./ngrok tcp 5900 >/dev/null 2>&1 &
 echo Ngrok Connect!
 echo -------------------------------
 sudo apt update -y > /dev/null 2>&1
