cde2ec5bfb584737879308f8732decce5af2db6d
RyuiMinh <167089278+RyuiMinh@users.noreply.github.com>
2024-07-22 19:35:02 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -12,16 +12,16 @@ fi
 if ! pgrep -f "ngrok tcp 5900" > /dev/null; then
     ./ngrok tcp 5900 --region ap &>/dev/null &
 fi
-echo Please wait for installing...
+echo "Please wait for installing..."
 echo "Installing QEMU (2-3m)..."
-echo Downloading Windows Disk...
+echo "Downloading Windows Disk..."
 if [ ! -f "lite7.qcow2" ]; then
     curl -L -o lite7.qcow2 https://app.vagrantup.com/thuonghai2711/boxes/WindowsQCOW2/versions/1.0.3/providers/qemu.box
 fi
 echo "Windows 7 x86 Lite"
-echo Your VNC IP Address:
+echo "Your VNC IP Address:"
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
-echo ====================
-echo    Script by CAAT
-echo ====================
+echo "===================="
+echo    "Script by CAAT"
+echo "===================="
 sudo qemu-system-x86_64 -vnc :0 -hda lite7.qcow2  -smp cores=2  -m 8192M -machine usb=on -device usb-tablet > /dev/null 2>&1
